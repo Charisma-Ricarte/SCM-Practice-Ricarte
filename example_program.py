895a930594c9dbf9780b5c27146883e4705cd966
Charisma-Ricarte <ann.marie.chr29@gmail.com>
2025-03-24 16:55:50 -0500

    Added subtract function and Updated print

--- example_program.py
+++ example_program.py
@@ -1,13 +1,17 @@
 # example_program.py
 
 def greet(name):
-    return f"Hello, {name}! Welcome to my example program!"
+    return f"Hello, I'm {name}! Welcome to my example program!"
 
 def add(a, b):
     if a < 0 or b < 0:
         return "Error: Cannot add negative numbers :("
     return a + b
 
+def subtract(a,b):
+    return a - b 
+
 if __name__ == "__main__":
-    print(greet("Alice"))
+    print(greet("Char"))
     print(f"2 + 3 = {add(2, 3)}")
+    print(f"5 - 3 = {subtract(5, 3)}")
